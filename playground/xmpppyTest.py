9153475b0f0143e5b529c0d529005904c3fb4d17
mt034 <mt034@hdm-stuttgart.de>
2009-09-27 03:11:01 +0200

    feedreader unicode problem solved. some example webengine sugar

--- playground/xmpppyTest.py
+++ playground/xmpppyTest.py
@@ -1,15 +1,34 @@
+# coding: utf-8
 import sys
 import xmpp
 
 
 
-def messageHandler(conn,msg):
-    content = unicode(msg.getBody())
-    sender = unicode(msg.getFrom())
-    print u"Sender: " + sender
-    print u"Content: " + content
 
-    send (content, sender)
+
+def messageHandler(conn,msg):
+    #content = msg.getBody().__str__().encode( "utf-8" )
+    #print type(msg.getBody().__str__())
+    #content = unicode(msg.getBody(), "utf8")#"iso-8859-1")
+
+    print "*"
+
+    if msg.getBody():
+
+
+        try:
+            content = msg.getBody().encode("iso-8859-1")
+        except (UnicodeDecodeError, UnicodeEncodeError):
+            content=u""
+            #conn.send(xmpp.Message(uin,reply.decode(kodow)))
+
+        #import code; code.interact(local=locals())
+
+        sender = unicode(msg.getFrom())
+        print "Sender: " + sender
+        print "Content: " + content
+
+        send (content, sender)
 
 def disconnectHandler(self):
     print ">>>>DISCONNECT HANDLER<<<<"
@@ -33,10 +52,12 @@ def goOn(conn):
         pass
 
 def send(msg, sender):
-    cl.send(xmpp.protocol.Message(sender,msg))
+    cl.send(xmpp.protocol.Message(sender,msg.decode("iso-8859-1")))
 
 
 def main():
+    print "Š"
+
     print "start"
     global recipient
 
