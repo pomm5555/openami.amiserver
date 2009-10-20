eb080e2d1bd8bfc9095d52a25b4b39f6a63913ac
root <root@vogsphere>
2009-10-20 21:37:13 +0000

    changes

--- PlugInsFolder/Audio/LastFM.py
+++ PlugInsFolder/Audio/LastFM.py
@@ -30,7 +30,7 @@ class LastFM(PlugIn):
         love = Container("cmd","Love","love")
         skip = Container("cmd","Skip","skip")
         ban = Container("cmd","Ban","ban")
-        quit = Container("cmd","Quit","quit")
+        stop = Container("cmd","Stop","stop")
         playlist = Container("cmd","Playlist","playlist")
         library = Container("cmd","Library","Library")
         neighbours = Container("cmd","Neighbours","neighbours")
@@ -38,7 +38,7 @@ class LastFM(PlugIn):
         love.setUse(self.love)
         skip.setUse(self.skip)
         ban.setUse(self.ban)
-        quit.setUse(self.quit)
+        stop.setUse(self.stop)
         playlist.setUse(self.playlist)
         library.setUse(self.library)
         neighbours.setUse(self.neighbours)
@@ -46,30 +46,38 @@ class LastFM(PlugIn):
         self.content.addChild(love)
         self.content.addChild(ban)
         self.content.addChild(skip)
-        self.content.addChild(quit)
+        self.content.addChild(stop)
         self.content.addChild(playlist)
         self.content.addChild(library)
         self.content.addChild(neighbours)
 
-    def quit(self,var):
-        os.system("echo 'quit' | nc " +self.host + " " + self.port)
+    def stop(self,var):
+        print "Stop"
+        os.system("echo 'stop' | nc " +host + " " + port)
 
     def neighbours(self,var):
+        print "LastFm Neighbours"
         os.system("echo 'play lastfm://user/"+user+"/neighbours' | nc " + host + " " + port)
+    
     def playlist(self,var):
+        print "LastFm Playlist"
         print host,port
         os.system("echo 'play lastfm://user/"+user+"/playlist' | nc " +host + " " + port)
 
     def library(self,var):
+        print "LastFm Library"
         os.system("echo 'play lastfm://user/"+user+"/library' | nc " +host + " " + port)
 
     def ban(self,var):
+        print "LastFm Ban"
         os.system("echo 'ban' | nc " +host + " " + port)
 
     def love(self,var):
+        print "LastFm Love"
         os.system("echo 'love' | nc " + host + " " + port)
 
     def skip(self,var):
+        print "LastFm Skip"
         os.system("echo 'skip' | nc " + host + " " + port)
 
 
