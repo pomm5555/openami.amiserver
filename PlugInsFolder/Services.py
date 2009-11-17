d3e629dc13ef8874760b39e2be4a113a997155ea
markus <markus@antennawireless.fritz.box>
2009-11-17 10:56:03 +0100

    now adding the Services Plugin, really

---
+++ PlugInsFolder/Services.py
@@ -0,0 +1,50 @@
+import os, re
+from AmiTree import Container
+from PlugIn import PlugIn
+from amiConfig import Config
+from Address import *
+from EventEngine import *
+
+class Services(PlugIn):
+
+    def __init__(self, token, configFile):
+        PlugIn.__init__(self)
+        self.architecture = "all"
+
+        #plugin itself
+        self.content = Container("plugin", token, "This is a Interface")
+
+        xmlC = Container("cmd", "Xml", "Get the Tree or Subtree as XML", self.getXml)
+        xmlC.rendering = Container.PLAIN
+
+        jqC = Container("cmd", "JqHtml", "Get the Tree or Subtree as JQ Touch HTML", self.getJq)
+        jqC.rendering = Container.PLAIN
+
+        test = Container("cmd", "test", "Get the Tree or Subtree as JQ Touch HTML", self.test)
+        #jqC.rendering = Container.PLAIN
+
+        self.content.addChild(xmlC)
+        self.content.addChild(jqC)
+        self.content.addChild(test)
+
+    def getXml(self, string=""):
+        if not string.__eq__(''):
+            address = Address(string).__str__()
+            tmp =  EventEngine.root.getByAddress(address).toXml()
+            return tmp
+        else:
+            return EventEngine.root.toXml()
+
+
+    def getJq(self, string=""):
+        if not string.__eq__(''):
+            address = Address(string).__str__()
+            tmp =  EventEngine.root.getByAddress(address).toJqHtml()
+            return tmp
+        else:
+            return EventEngine.root.toJqHtml()
+
+    def test(self, string=""):
+        addr = Address("servant@jabber.org/Services/JqHtml").__str__()
+        link = "servant@jabber.org/Status"
+        return EventEngine.root.getByAddress(addr).use(link)
