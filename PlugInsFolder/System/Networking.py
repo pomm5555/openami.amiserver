957a36e667895b25dc1ac649440d1dbf9949b565
markus <markus@antennawireless.fritz.box>
2009-11-19 08:47:03 +0100

    added client networking feature: Clients send Broadcasts, Masters listen to it.

---
+++ PlugInsFolder/System/Networking.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+from AmiTree import *
+from PlugIn import PlugIn
+from amiConfig import Config
+from EventEngine import EventEngine
+from Address import Address
+import time, socket, urllib2
+
+class ClientListener(PlugIn):
+
+
+    def __init__(self, token, configFile):
+        PlugIn.__init__(self)
+        # set plugins "hardware" architecture for system dependencies
+        self.architecture = "all"
+
+        # create plugin itself
+        self.content = ThreadContainer("plugin", token, "Listening to Broadcasts for Client amiServers")
+        
+        if Config.get("server", "master").__eq__("on"):
+            method = self.listen
+            ThreadName = "ClientListener"
+        else:
+            method = self.send
+            ThreadName = "Broadcaster"
+            
+            
+        self.content.setDo(method)
+        # dont know wheather it works, shold set the name of the thread
+        self.content.name = ThreadName
+
+        # start thread
+        self.content.start()
+
+
+    def listen(self):
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        my_socket.bind(('', 8081))
+    
+        print 'start ClientListener ...'
+    
+        while True :
+            clientName , remoteIp = my_socket.recvfrom(8192)
+            print 'message (%s) from : %s' % (str(clientName), remoteIp[0])
+            self.root().addChild(WebBuddyContainer("remoteInstanceView", clientName, remoteIp[0]))
+            
+            
+    def send(self):
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        print 'start Broadcasting ...'
+    
+        while True :
+            my_socket.sendto(Config.get("server", "token"), ('<broadcast>' , 8081))
+            time.sleep(60)
+
+
+    def toHtml(self):
+        if self.visible:
+            result = ""
+            for k, v in self.content.items():
+                result += v.toHtml()
+
+            #if result.__eq__(""):
+            #    return "<li><a href=\""+self.getAddress()+"\">"+self.token+"</a>"+result+"</li>"
+            return "<ul><li><a href=\"" + self.getAddress() + "\">" + self.token + "</a> L</li>" + result + "</ul>"
+            
+        else:
+            return ""
+
+    def use(self, string=""):
+        result = ""
+        for elem in self.log:
+            result += "\n" + elem
+        return result
+    
+class WebBuddyContainer(Container):
+    def __init__(self, type, token, information):
+        Container.__init__(self, type, token, information)
+        self.rendering = Container.PLAIN
+        addr = "http://" + self.information + ":8080/" + self.token + "/Services/JqHtml" + "?string=" + self.token
+        print addr
+        self.answer = urllib2.urlopen(addr).read()
+        print self.answer
+
+    def use(self, msg=""):
+        return self.answer
+    
--- plugLoder.py
+++ plugLoder.py
@@ -1,8 +1,8 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
-__author__="markus"
-__date__ ="$Aug 20, 2009 12:22:08 AM$"
+__author__ = "markus"
+__date__ = "$Aug 20, 2009 12:22:08 AM$"
 from AmiTree import Container
 from amiConfig import Config
 import os
@@ -12,7 +12,7 @@ class PlugIns:
     def __init__(self, pluginsFolder, configFile):
 
         #absolute folder to plugins
-        absPluginsFolder = Config.absPath+"/"+Config.get("Plugins", "PlugInsFolder")
+        absPluginsFolder = Config.absPath + "/" + Config.get("Plugins", "PlugInsFolder")
 
         #creating list whrer all plugins are loaded in
         self.content = []
@@ -42,7 +42,7 @@ class PlugIns:
 
 
         # get all elements in folder into list
-        pluginFiles =  os.listdir(PluginsPath)
+        pluginFiles = os.listdir(PluginsPath)
         #print pluginFiles
         #print PluginsPath, PackagePath
 
@@ -51,23 +51,30 @@ class PlugIns:
         for elem in pluginFiles:
             # add plugin files
             if elem[-3:].__eq__(".py") and not (elem[:1].__eq__("_")):
-                print "*** loading: "+ PackagePath+ "." +elem
+                print "*** loading: " + PackagePath + "." + elem
                 #print("from "+PackagePath+"."+elem[:-3]+" import "+elem[:-3])
-                exec("from "+PackagePath+"."+elem[:-3]+" import "+elem[:-3])
+                exec("from " + PackagePath + "." + elem[:-3] + " import " + elem[:-3])
                 #print("plugin = "+elem[:-3]+"(\""+elem[:-3]+"\", \""+configFile+"\")")
-                exec("plugin = "+elem[:-3]+"(\""+elem[:-3]+"\", \""+configFile+"\")")
+                exec("plugin = " + elem[:-3] + "(\"" + elem[:-3] + "\", \"" + configFile + "\")")
                 #print("result.append(plugin.getTree())")
                 #print plugin.architecture
 
-                if plugin.architecture.__eq__("all") or plugin.architecture.__eq__(Config.architecture):
+                # If its a Master Plugin, it only should loaded by Masters, else, check architecture
+                if plugin.master == True:
+                    #Check if it amiServer is configured as master
+                    if Config.get("server", "master").__eq__("on"):
+                        result.append(plugin.getTree())
+
+                elif plugin.architecture.__eq__("all") or plugin.architecture.__eq__(Config.architecture):
+                    
                     result.append(plugin.getTree())
 
             # add folders as plugin
-            if os.path.isdir(PluginsPath+"/"+elem):
-                print "+++ loding folder: "+PluginsPath+"."+elem
+            if os.path.isdir(PluginsPath + "/" + elem):
+                print "+++ loding folder: " + PluginsPath + "." + elem
                 tmpcont = Container("folder", elem, "this is the tree representation of a folder")
                 tmpcont.setUse(self.display)
-                tmpcont.addChildList(self.loadPlugins(PluginsPath+"/"+elem, PackagePath+"."+elem, configFile))
+                tmpcont.addChildList(self.loadPlugins(PluginsPath + "/" + elem, PackagePath + "." + elem, configFile))
                 result.append(tmpcont)
   
         return result
