3d9adf6076b0905f381f00959a031b0d32a0918c
markus <markus@antennawireless.fritz.box>
2009-12-16 19:44:25 +0100

    actualized jqtouch, added bottom navigationbar

--- WebEngine.py
+++ WebEngine.py
@@ -24,32 +24,47 @@ class WebServer():
 
     def index(self):
 
-        
+        navigationbar = '''
+        <span id='navigationbar'>
+            <ul>
+                <li><a href='geheim@jabber.org/Dashboard'>Dashboard</a></li>
+                <li><a href='#geheim_jabber_org'>Tree</a></li>
+                <li><a href='#geheim_jabber_org'>Ami Info</a></li>
+                <li><a href='#geheim_jabber_org'>Network Info</a></li>
+            </ul>
+        </span>
+        '''
 
         addr = Config.get("server", "token")
 
         head = '''
-        <script type="text/javascript" src="''' + addr + '''/Filesystem/html/jquery.js"></script>
-        <script src="''' + addr + '''/Filesystem/html/jqtouch/jqtouch.min.js" type="application/x-javascript" charset="utf-8"></script>
-        <style type="text/css" media="screen">@import "''' + addr + '''/Filesystem/html/jqtouch/jqtouch.min.css";</style>
-        <style type="text/css" media="screen">@import "''' + addr + '''/Filesystem/html/themes/jqt/theme.css";</style>
-        <script type="text/javascript" charset="utf-8">
-        $(document).ready(function(){
-            $.jQTouch({
-                icon: 'jqtouch.png',
-                statusBar: 'black-translucent',
-                preloadImages: [
-                    ' ''' + addr + '''/Filesystem/html/themes/jqt/img/back_button_clicked.png',
-                    ' ''' + addr + '''/Filesystem/html/themes/jqt/img/button_clicked.png'
-                    ]
-            });
-        });
-        </script>
+        <meta charset="UTF-8" />
 
+        <style type="text/css" media="screen">@import "'''+addr+'''/Filesystem/html/jqtouch/jqtouch.css";</style>
+        <style type="text/css" media="screen">@import "'''+addr+'''/Filesystem/html/themes/jqt/theme.css";</style>
+        <style type="text/css" media="screen">@import "'''+addr+'''/Filesystem/html/ami.css";</style>
+        <script src="'''+addr+'''/Filesystem/html/jqtouch/jquery.1.3.2.min.js" type="text/javascript" charset="utf-8"></script>
+        <script src="'''+addr+'''/Filesystem/html/jqtouch/jqtouch.js" type="application/x-javascript" charset="utf-8"></script>
+        <script src="'''+addr+'''/Filesystem/html/extensions/jqt.offline.js" type="application/x-javascript" charset="utf-8"></script>
+        <script type="text/javascript" charset="utf-8">
+            var jQT = new $.jQTouch({
+                icon: ' '''+addr+'''/Filesystem/html/images/appIcon.png',
+                addGlossToIcon: true,
+                startupScreen: ' '''+addr+'''/Filesystem/html/images/startup.png',
+                statusBar: 'black'
+            });
+
+            $(function(){
+                // Custom Javascript (onReady)
+            });
+        </script>
+        <style type="text/css" media="screen">
+            /* Custom Style */
+        </style>
         '''
 
         result = self.root.toJqHtml()
-        return "<html><head>" + head + "</head><body>" + result + "</body></html>"
+        return '<html manifest="/'+addr+'/Filesystem/html/cache.manifest"><head>' + head + '</head><body>' + result + navigationbar + '</body></html>'
     index.exposed = True
 
 
@@ -57,11 +72,6 @@ class WebServer():
     # This part of the program parses the URL which was called by a browser
     # and calls the use method...
     def default(self, *args, **kwargs):
-        
-        #print "BEGIN"
-        #print args
-        #print kwargs
-        #print "END"
         
         str=""
         for elem in args:
@@ -106,10 +116,3 @@ class WebServer():
             
             
     default.exposed = True
-
-
-    
-
-
-if __name__ == "__main__":
-    we = XMPPEngine("hallo")
