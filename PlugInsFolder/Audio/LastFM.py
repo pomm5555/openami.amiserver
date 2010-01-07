708a544eda25957af9f4de242e5b7f276e096dd6
markus <markus@antenna.fritz.box>
2010-01-07 16:11:19 +0100

    more improved audioplayer/lastfm plugin....

--- PlugInsFolder/Audio/LastFM.py
+++ PlugInsFolder/Audio/LastFM.py
@@ -83,47 +83,47 @@ class LastFM(PlugIn):
         self.content.addChild(TextfieldContainer("ui", "SimilarArtist", "PlaySimilarArtiest", target=Config.jid+"/Audio/LastFM/Similar"))
 
     def stop(self,var):
-        print "Stop"
+        #print "Stop"
         os.system("echo 'stop' | nc " +host + " " + port)
 
     def neighbours(self,var):
-        print "LastFm Neighbours"
+        #print "LastFm Neighbours"
         os.system("echo 'play lastfm://user/"+user+"/neighbours' | nc " + host + " " + port)
     
     def playlist(self,var):
-        print "LastFm Playlist"
-        print host,port
+        #print "LastFm Playlist"
+        #print host,port
         os.system("echo 'play lastfm://user/"+user+"/playlist' | nc " +host + " " + port)
 
     def library(self,var):
-        print "LastFm Library"
+        #print "LastFm Library"
         os.system("echo 'play lastfm://user/"+user+"/library' | nc " +host + " " + port)
 
     def ban(self,var):
-        print "LastFm Ban"
+        #print "LastFm Ban"
         os.system("echo 'ban' | nc " +host + " " + port)
 
     def love(self,var):
-        print "LastFm Love"
+        #print "LastFm Love"
         os.system("echo 'love' | nc " + host + " " + port)
 
     def skip(self,var):
-        print "LastFm Skip"
+        #print "LastFm Skip"
         os.system("echo 'skip' | nc " + host + " " + port)
 
 
     def similar(self, artist):
-        print "LastFm similar"
+        #print "LastFm similar"
         # lastfm://artist/cher/similarartists
         os.system("echo 'play lastfm://artist/"+artist+"/similarartists' | nc " + host + " " + port)
         
     def tag(self, tag):
-        print "LastFm Tag"
+        #print "LastFm Tag"
         
         os.system("echo 'play lastfm://tag/"+tag+"' | nc " + host + " " + port)
 
     def getNp(self,var):
-        print "LastFM  get now Playing"
+        #print "LastFM  get now Playing"
         #info = pipe = os.popen("curl http://192.168.1.131","r")
         info = os.popen('echo info | nc ' + host + ' ' + port)
 	return info.read()
@@ -133,7 +133,7 @@ class LastFM(PlugIn):
 	#np = pipe = os.popen("curl http://192.168.1.131","r")
         np = np.read()
         (artist, song, album) = np.split(' - ')
-        print artist, song, album
+        #print artist, song, album
         np = artist
 
         if not self.lastCover == artist+song+album:
@@ -142,28 +142,28 @@ class LastFM(PlugIn):
 
             ecs.setLicenseKey('AKIAIICBON46BQIRCOUQ')
             ecs.setSecretKey('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')
-            print "** Searching Amazon for "+artist+" "+album
+            #print "** Searching Amazon for "+artist+" "+album
             search = ecs.ItemSearch(Keywords='Music', SearchIndex='Music',Artist=artist, Title=album, ResponseGroup='Images')
             img=search.next().LargeImage.URL
-            print img
+            #print img
             self.lastImg = img
-            print "LastFM getCoverArt!!!!!!!!!!!"
+            #print "LastFM getCoverArt!!!!!!!!!!!"
 
             return img
         else:
-            print 'LOADING CACHED IMAGE FOR COVERART!!!'+self.lastImg
+            #print 'LOADING CACHED IMAGE FOR COVERART!!!'+self.lastImg
             return self.lastImg
 
     def getCoverArtImage(self,var):
         addr = Address('/Audio/LastFM/CoverArt')
         imgurl = self.root().getByAddress(addr.__str__()).use()
-        print imgurl
+        #print imgurl
         img = urllib2.urlopen(imgurl).read()
         return img
 
 
     def getState(self, var):
-        print "LastFM getState"
+        #print "LastFM getState"
         np = os.popen('echo info | nc ' + host + ' ' + port).read()
         if np == ' -  - ':
           return "not playing"
